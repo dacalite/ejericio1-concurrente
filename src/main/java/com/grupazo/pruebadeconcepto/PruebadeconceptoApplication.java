745c9c5cf22aadd57e737ba8e7511640f4e8d389
Sergio Dacal Rodriguez <sdacalrodriguez@gmail.com>
2024-09-17 00:02:44 +0200

    first commit: initializr template

---
+++ src/main/java/com/grupazo/pruebadeconcepto/PruebadeconceptoApplication.java
@@ -0,0 +1,13 @@
+package com.grupazo.pruebadeconcepto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebadeconceptoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebadeconceptoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/grupazo/pruebadeconcepto/PruebadeconceptoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupazo.pruebadeconcepto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebadeconceptoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
